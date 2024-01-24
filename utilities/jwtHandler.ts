564d6e98d3f176d130ebe608b4d6358b17e113c9
ajibz <alexanderprofesssor@gmail.com>
2024-01-24 09:33:48 +0100

    JWT error handling

--- utilities/jwtHandler.ts
+++ utilities/jwtHandler.ts
@@ -2,24 +2,44 @@ import jwt from 'jsonwebtoken'
 import userRepo from '../repository/UserRepository'
 import { HttpStatus } from './HttpstatusCode'
   function generateJWT(payload: object):string{
-    let Secret = <string>process.env.JWT_SECRET
-    return jwt.sign(payload,Secret,{expiresIn:'1h'})
+    try{
+      let Secret = <string>process.env.JWT_SECRET
+      return jwt.sign(payload,Secret,{expiresIn:'1h'})
+
+    }catch(error){
+      console.log('generateJWT error:',error)
+      return ''
+    }
+   
 }
 
   async function generateRefreshToken(phone:string):Promise<string>{
-    let Secret = <string>process.env.JWT_SECRET
+    try{
+      let Secret = <string>process.env.JWT_SECRET
     let token =  jwt.sign({data:'just a refresh token'},Secret,{expiresIn:'7days'})
     let result = await new userRepo().UpdateUserRefreshToken(phone,token)
     if(result.toLowerCase() !== HttpStatus.STATUS_SUCCESS){
       return ''
     }
     return token;
+
+    }catch(error){
+      console.log('generateRefreshToken error:',error)
+      return ''
+    }
+    
 }
 
  async function IsValidToken(token:string):Promise<boolean>{
+  try{
     let Secret = <string>process.env.JWT_SECRET
-   let payload = await jwt.verify(token,Secret)
-   return payload ? true : false
+    let payload = await jwt.verify(token,Secret)
+    return payload ? true : false
+  }catch(error){
+    console.log('Jwt Validation error:',error)
+   return false
+  }
+   
 }
 
 export default {
